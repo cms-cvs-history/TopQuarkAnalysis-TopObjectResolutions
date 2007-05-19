edabe0eb9f0cb783bd9a7a4bf5c504c2bb05d198
heyninck <sha1-082da020e7327f374b7da66709d8d266b70884a2@cern.ch>
2007-05-19 10:01:57 +0000

    changes committed as Petra suggested. However, this code needs an update once we ran on more statistics

--- bin/ResolutionBinningTest.cpp
+++ bin/ResolutionBinningTest.cpp
@@ -27,6 +27,10 @@ TH2F *lJetEtEtaBinning;
 TH2F *bJetEtEtaBinning;
 TH2F *muonEtEtaBinning;
 TH2F *metEtEtaBinning;
+TH1F *lJetsBinning;//to check if code is working
+TH1F *bJetsBinning;//to check if code is working
+TH1F *mBinning;//to check if code is working
+TH1F *metBinning;//to check if code is working
 TFile *outfile;
 
 //variables
@@ -35,6 +39,8 @@ const static int nrEtabins = 10;
 const static int maxEt = 200;
 const static int maxEta = 200;
 const static float truemaxEta = 2.5;
+const static int nrsel = 100000; // fix number, otherwise array possibly to big !! 
+int icount;
 int totljets;
 int totbjets;
 int totm;
@@ -49,9 +55,14 @@ void defineSolutionHistos() {
   bJetEtEtaBinning = new TH2F("bJetEtEtaBinning","to bin the Et-Eta of b jets",40,0,200,20,0,3);
   muonEtEtaBinning = new TH2F("muonEtEtaBinning","to bin the Et-Eta of muons",40,0,200,20,0,3);
   metEtEtaBinning  = new TH2F("metEtEtaBinning","to bin the Et-Eta of the neutrino",40,0,200,20,0,3);
+  lJetsBinning 	   = new TH1F("lJetsBinning","same number of lJets in each bin ",50,0,0.05);//to check if code is working
+  bJetsBinning 	   = new TH1F("bJetsBinning","same number of bJets in each bin ",50,0,0.05);//to check if code is working
+  mBinning 	   = new TH1F("mBinning","same number of muons in each bin ",50,0,0.05);//to check if code is working
+  metBinning 	   = new TH1F("metBinning","same number of neutrinos in each bin ",50,0,0.5);//to check if code is working
 }
 
 void defineVariables(){
+  icount = 0;
   totljets = 0;
   totbjets = 0;
   totm = 0;
@@ -78,7 +89,9 @@ int main() {
   outfile = new TFile("../data/EtEtaBinning.root", "RECREATE");;
   defineSolutionHistos();
   defineVariables();
-   
+  
+  cout << "I am defining the variables" << endl;
+  
   //lJet variables
   float EtlJetbinup[nrEtbins+1];
   EtlJetbinup[0]=0.;
@@ -88,7 +101,8 @@ int main() {
   EtalJetbinup[0]=0.;
   EtalJetbinup[nrEtabins]=truemaxEta;
   int countEtalJet[maxEta+1];
-  float etljets[2],etaljets[2];
+  float etljets[2][nrsel],etaljets[2][nrsel];
+  int countEtEtalJet[nrEtbins][nrEtabins]; //to check if code is working
   
   //bJet variables
   float EtbJetbinup[nrEtbins+1];
@@ -99,7 +113,8 @@ int main() {
   EtabJetbinup[0]=0.;
   EtabJetbinup[nrEtabins]=truemaxEta;
   int countEtabJet[maxEta+1];
-  float etbjets[2],etabjets[2];
+  float etbjets[2][nrsel],etabjets[2][nrsel];
+  int countEtEtabJet[nrEtbins][nrEtabins];//to check if code is working
   
   //muon variables
   float Etmbinup[nrEtbins+1];
@@ -110,14 +125,16 @@ int main() {
   Etambinup[0]=0.;
   Etambinup[nrEtabins]=truemaxEta;
   int countEtam[maxEta+1];
-  float etm,etam;
-   
+  float etm[nrsel],etam[nrsel];
+  int countEtEtam[nrEtbins][nrEtabins];//to check if code is working
+  
   //neutrino variables
   float Etmetbinup[nrEtbins+1];
   Etmetbinup[0]=0.;
   Etmetbinup[nrEtbins]=(float)maxEt;
   int countEtmet[maxEt+1];
-  float etmet;
+  float etmet[nrsel];
+  int countEtmettest[nrEtbins];//to check if code is working
   
   //initialisation of the variables
   for(int i = 0; i <= maxEt; i++){
@@ -135,6 +152,7 @@ int main() {
   TString ft = "file:/beo5/pvmulder/CMSSW/src/myTopAnalyses/TopEtEtaBinning/test/LooserCutsEtEtaBinning.root"; 
   if (!gSystem->AccessPathName(ft)) {
     TFile *file = TFile::Open(ft);
+    cout << "I am opening the rootfile" << endl;
     TTree * events = dynamic_cast<TTree*>( file->Get( "Events" ) );
     assert( events != 0 );
     TBranch * solsbranch = events->GetBranch( "TtSemiEvtSolutions_solutions__LooserCutsEtEtaBinning.obj" );
@@ -142,7 +160,9 @@ int main() {
 
     vector<TtSemiEvtSolution> sols;
     solsbranch->SetAddress( & sols );
-      
+    
+    cout << "now I will choose the best solution for each event and calculate the nr of objects for certain Et and Eta" << endl; 
+    
     int nev = events->GetEntries();
     for( int ev = 0; ev < nev; ++ ev ) { 
       solsbranch -> GetEntry( ev );
@@ -154,55 +174,62 @@ int main() {
         }     
         if(bestSol>-1){ 
 	  AnalyseSolution(sols[bestSol]);
+	  icount++;
 	  
-	  //lJet et and eta
-	  etljets[0] = sols[bestSol].getCalHadp().et();
-	  etljets[1] = sols[bestSol].getCalHadq().et();
-	  etaljets[0] = fabs(sols[bestSol].getCalHadp().eta());
-	  etaljets[1] = fabs(sols[bestSol].getCalHadq().eta());
-	  //bJet et and eta
-	  etbjets[0] = sols[bestSol].getCalHadb().et();
-	  etbjets[1] = sols[bestSol].getCalLepb().et();
-	  etabjets[0] = fabs(sols[bestSol].getCalHadb().eta());
-	  etabjets[1] = fabs(sols[bestSol].getCalLepb().eta());
-	  //muon et and eta
-	  etm = sols[bestSol].getRecLepm().et();
-	  etam = fabs(sols[bestSol].getRecLepm().eta());
-	  //neutrino et
-	  etmet = sols[bestSol].getRecLepn().et();
-	 
-	  for(int j = 0; j <= maxEt; j++){
-	    for(int i=0; i<2; i++){
-	      if((etljets[i] <= (float)j) && (etaljets[i] <= truemaxEta)) countEtlJet[j]++;
-	      if((etbjets[i] <= (float)j) && (etabjets[i] <= truemaxEta)) countEtbJet[j]++;
-	    }
-	    if((etm <= (float)j) && (etam <= truemaxEta)) countEtm[j]++;
-	    if((etmet <= (float)j)) countEtmet[j]++;
-	  }     
- 	  for(int j = 0; j <= maxEta; j++){
-	    float trueEtaCut = (float)j*truemaxEta/maxEta;
-	    for(int i=0; i<2; i++){
-	      if((etaljets[i] <= trueEtaCut) && (etljets[i] <= (float)maxEt)) countEtalJet[j]++;
-	      if((etabjets[i] <= trueEtaCut) && (etbjets[i] <= (float)maxEt)) countEtabJet[j]++;
-	    }
-	    if((etam <= trueEtaCut) && (etm <= (float)maxEt)) countEtam[j]++;
-	  } 
-	  //total number of ljets
-	  if((etljets[0] <= (float)maxEt) && (etaljets[0] <= truemaxEta)) totljets++;
-	  if((etljets[1] <= (float)maxEt) && (etaljets[1] <= truemaxEta)) totljets++;
-	  //total number of bjets
-	  if((etbjets[0] <= (float)maxEt) && (etabjets[0] <= truemaxEta)) totbjets++;
-	  if((etbjets[1] <= (float)maxEt) && (etabjets[1] <= truemaxEta)) totbjets++;
-	  //total number of muons
-	  if((etm <= (float)maxEt) && (etam <= truemaxEta)) totm++;
-	  //total number of neutrinos
-	  if((etmet <= (float)maxEt)) totmet++;
+	  if(icount < nrsel){
+	    //lJet et and eta
+	    etljets[0][icount] = sols[bestSol].getCalHadp().et();
+	    etljets[1][icount] = sols[bestSol].getCalHadq().et();
+	    etaljets[0][icount] = fabs(sols[bestSol].getCalHadp().eta());
+	    etaljets[1][icount] = fabs(sols[bestSol].getCalHadq().eta());
+	    //bJet et and eta
+	    etbjets[0][icount] = sols[bestSol].getCalHadb().et();
+	    etbjets[1][icount] = sols[bestSol].getCalLepb().et();
+	    etabjets[0][icount] = fabs(sols[bestSol].getCalHadb().eta());
+	    etabjets[1][icount] = fabs(sols[bestSol].getCalLepb().eta());
+	    //muon et and eta
+	    etm[icount] = sols[bestSol].getRecLepm().et();
+	    etam[icount] = fabs(sols[bestSol].getRecLepm().eta());
+	    //neutrino et
+	    etmet[icount] = sols[bestSol].getRecLepn().et();
+	  
+	    for(int j = 0; j <= maxEt; j++){
+	      for(int i=0; i<2; i++){
+	        if((etljets[i][icount] <= (float)j) && (etaljets[i][icount] <= truemaxEta)) countEtlJet[j]++;
+	        if((etbjets[i][icount] <= (float)j) && (etabjets[i][icount] <= truemaxEta)) countEtbJet[j]++;
+	      }
+	      if((etm[icount] <= (float)j) && (etam[icount] <= truemaxEta)) countEtm[j]++;
+	      if((etmet[icount] <= (float)j)) countEtmet[j]++;
+	    }     
+ 	    for(int j = 0; j <= maxEta; j++){
+	      float trueEtaCut = (float)j*truemaxEta/maxEta;
+	      for(int i=0; i<2; i++){
+	        if((etaljets[i][icount] <= trueEtaCut) && (etljets[i][icount] <= (float)maxEt)) countEtalJet[j]++;
+	        if((etabjets[i][icount] <= trueEtaCut) && (etbjets[i][icount] <= (float)maxEt)) countEtabJet[j]++;
+	      }
+	      if((etam[icount] <= trueEtaCut) && (etm[icount] <= (float)maxEt)) countEtam[j]++;
+	    } 
+	    //total number of ljets
+	    if((etljets[0][icount] <= (float)maxEt) && (etaljets[0][icount] <= truemaxEta)) totljets++;
+	    if((etljets[1][icount] <= (float)maxEt) && (etaljets[1][icount] <= truemaxEta)) totljets++;
+	    //total number of bjets
+	    if((etbjets[0][icount] <= (float)maxEt) && (etabjets[0][icount] <= truemaxEta)) totbjets++;
+	    if((etbjets[1][icount] <= (float)maxEt) && (etabjets[1][icount] <= truemaxEta)) totbjets++;
+	    //total number of muons
+	    if((etm[icount] <= (float)maxEt) && (etam[icount] <= truemaxEta)) totm++;
+	    //total number of neutrinos
+	    if((etmet[icount] <= (float)maxEt)) totmet++;
+	  }
 	} 
       } 
     }
     file->Close();
   } 
   
+  if(icount > nrsel) icount = nrsel;
+  
+  cout << "now I will calculate the EtEtabins" << endl; 
+    
   for(int i=1; i<nrEtbins; i++){
     EtlJetbinup[i]=0.;
     EtbJetbinup[i]=0.;
@@ -225,6 +252,7 @@ int main() {
       if(((float)(countEtam[j])/((float)totm) > ((float)(i)/(float)(nrEtabins))) && (Etambinup[i]==0.)) Etambinup[i]=(float)j*truemaxEta/maxEta;
     }
   } 
+  
   cout << "in all the EtEtabins there is ~"<< 100/(float)(nrEtbins*nrEtabins) <<"% of the objects" << endl;
   cout << " " << endl;
   cout << "light jets | Etbins | Etabins"<< endl; 
@@ -250,6 +278,41 @@ int main() {
   for(int i=0; i<nrEtbins; i++){ 
     cout << "metbin         " << Etmetbinup[i]  << "    no eta-bin"    << endl;
   }
+  
+  cout << "now I will make the histograms which show that there is a peak around  ~"<< 1/(float)(nrEtbins*nrEtabins) << endl;
+  
+  //to check if code is working: lines 256 -> 287
+  for(int iEtbin=1; iEtbin < nrEtbins; iEtbin++){
+    for(int iEtabin=1; iEtabin < nrEtabins; iEtabin++){
+      countEtEtalJet[iEtbin][iEtabin]=0;
+      countEtEtabJet[iEtbin][iEtabin]=0;
+      countEtEtam[iEtbin][iEtabin]=0;
+    }
+    countEtmettest[iEtbin]=0;
+  }
+	
+  for(int iEtbin=1; iEtbin <= nrEtbins; iEtbin++){
+    for(int iEtabin=1; iEtabin <= nrEtabins; iEtabin++){
+      for(int i=1; i <= icount; i++){
+	for(int ijet=0; ijet<2; ijet++){
+	  if((etljets[ijet][i] > EtlJetbinup[iEtbin-1]) && (etljets[ijet][i] < EtlJetbinup[iEtbin]) && (etaljets[ijet][i] > EtalJetbinup[iEtabin-1]) && (etaljets[ijet][i] <
+	    EtalJetbinup[iEtabin])) countEtEtalJet[iEtbin][iEtabin]++;  
+	  if((etbjets[ijet][i] > EtbJetbinup[iEtbin-1]) && (etbjets[ijet][i] < EtbJetbinup[iEtbin]) && (etabjets[ijet][i] > EtabJetbinup[iEtabin-1]) && (etabjets[ijet][i] <
+	    EtabJetbinup[iEtabin])) countEtEtabJet[iEtbin][iEtabin]++;
+	}
+	if((etm[i] > Etmbinup[iEtbin-1]) && (etm[i] < Etmbinup[iEtbin]) && (etam[i] > Etambinup[iEtabin-1]) && (etam[i] < Etambinup[iEtabin])) countEtEtam[iEtbin][iEtabin]++;
+      }
+      lJetsBinning->Fill((float)countEtEtalJet[iEtbin][iEtabin]/(float)totljets);
+      bJetsBinning->Fill((float)countEtEtabJet[iEtbin][iEtabin]/(float)totbjets);   
+      mBinning    ->Fill((float)countEtEtam[iEtbin][iEtabin]/(float)totm);   
+    }
+  }
+  for(int iEtbin=1; iEtbin <= nrEtbins; iEtbin++){
+    for(int i=1; i <= icount; i++){
+      if((etmet[i] > Etmetbinup[iEtbin-1]) && (etmet[i] < Etmetbinup[iEtbin])) countEtmettest[iEtbin]++;
+    }
+    metBinning    ->Fill((float)countEtmettest[iEtbin]/(float)totmet); 
+  } 
     
   gROOT->SetBatch();
   gROOT->SetStyle("Plain");  
